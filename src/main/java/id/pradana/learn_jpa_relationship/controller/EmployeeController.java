839392a5c39ac4d54d2149eb4787fc4e5e3ddf48
pradana4648 <wakwaujaim@gmail.com>
2023-08-19 21:56:12 +0700

    refactor: update @Controller to @RestController

--- src/main/java/id/pradana/learn_jpa_relationship/controller/EmployeeController.java
+++ src/main/java/id/pradana/learn_jpa_relationship/controller/EmployeeController.java
@@ -3,22 +3,21 @@ package id.pradana.learn_jpa_relationship.controller;
 import id.pradana.learn_jpa_relationship.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
   @Autowired
   private EmployeeService service;
 
   @PostMapping("/employees")
-  @ResponseBody
   public ResponseEntity<?> getAllEmployee(
       @RequestParam(name = "sortBy", required = false) String sortBy,
       @RequestParam(name = "dir", required = false) String direction,
@@ -29,7 +28,6 @@ public class EmployeeController {
   }
 
   @GetMapping("/employees/{id}")
-  @ResponseBody
   public ResponseEntity<?> getEmployeeById(@PathVariable("id") long id) {
     return service.getEmployeeById(id);
   }
