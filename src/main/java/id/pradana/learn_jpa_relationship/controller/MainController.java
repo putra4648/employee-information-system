c7508d3ef68c13c372f2a00ec635568449825930
pradana4648 <wakwaujaim@gmail.com>
2023-08-27 20:17:10 +0700

    fix: change limit export data to 1k

--- src/main/java/id/pradana/learn_jpa_relationship/controller/MainController.java
+++ src/main/java/id/pradana/learn_jpa_relationship/controller/MainController.java
@@ -1,10 +1,12 @@
 package id.pradana.learn_jpa_relationship.controller;
 
+import id.pradana.learn_jpa_relationship.filter.EmployeeFilterDTO;
+import id.pradana.learn_jpa_relationship.service.EmployeeService;
+import id.pradana.learn_jpa_relationship.view.ExportExcelView;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,10 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import id.pradana.learn_jpa_relationship.filter.EmployeeFilterDTO;
-import id.pradana.learn_jpa_relationship.service.EmployeeService;
-import id.pradana.learn_jpa_relationship.view.ExportExcelView;
-
 @Controller
 public class MainController {
   @Autowired
@@ -24,6 +22,7 @@ public class MainController {
 
   @GetMapping("/")
   public ModelAndView main(ModelMap model) {
+    model.put("limit_export", 1000);
     return new ModelAndView("index", model);
   }
 
@@ -33,8 +32,7 @@ public class MainController {
   }
 
   @GetMapping("/export")
-  public ModelAndView exportPage(
-      @RequestParam(name = "employee_id", required = false) String id,
+  public ModelAndView exportPage(@RequestParam(name = "employee_id", required = false) String id,
       @RequestParam(name = "employee_fullname", required = false) String fullname,
       @RequestParam(name = "employee_birthdate", required = false) String birthdate,
       @RequestParam(name = "employee_hiredate", required = false) String hiredate) throws ParseException {
